c3ff56f5fddbf6ca988d038f63aa6594dba16969
unknown <alabdullatif.sara@gmail.com>
2019-02-18 17:21:58 +0300

    Sara_Alabdullatif_Clock_HW_updated

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -27,39 +27,74 @@ $(document).ready(function(){
 
     }
 
-})
+      
+    setInterval(digitalClock12, 1000)
+
+    function digitalClock12(){
+        var currentTime = new Date ( );
+
+        var currentHours = currentTime.getHours ( );
+        var currentMinutes = currentTime.getMinutes ( );
+        var currentSeconds = currentTime.getSeconds ( );
+
+        // Pad the minutes and seconds with leading zeros, if required
+        currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+        currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+        // Choose either "AM" or "PM" as appropriate
+        var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+
+        // Convert the hours component to 12-hour format if needed
+        currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+        // Convert an hours component of "0" to "12"
+        currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+        // Compose the string for display
+        var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
 
 
-// $(document).ready(function(){
+        // // Update the time display
+        $(".digi_clock12").html(currentTimeString)
+    }
+
+    setInterval(digitalClock24, 1000)
+
+    function digitalClock24(){
+        var currentTime = new Date ( );
+
+        var currentHours = currentTime.getHours ( );
+        var currentMinutes = currentTime.getMinutes ( );
+        var currentSeconds = currentTime.getSeconds ( );
+
+        // Pad the minutes and seconds with leading zeros, if required
+        currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+        currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+        // Choose either "AM" or "PM" as appropriate
+        var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+
+        // // Convert the hours component to 12-hour format if needed
+        // currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+        // // Convert an hours component of "0" to "12"
+        // currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+        // Compose the string for display
+        var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+
+        // Update the time display
+        $(".digi_clock24").html(currentTimeString)
+    }
+    
+    $('.digi_clock24').hide()
+    $('.digi_clock12').show()
+
+    $('#btn').click(function(){
         
-//     setInterval(digitalClock, 1000)
+        $('.digi_clock24').toggle()
+        $('.digi_clock12').toggle()
 
-//     function digitalClock(){
-//     var currentTime = new Date ( );
-
-//     var currentHours = currentTime.getHours ( );
-//     var currentMinutes = currentTime.getMinutes ( );
-//     var currentSeconds = currentTime.getSeconds ( );
-
-//     // Pad the minutes and seconds with leading zeros, if required
-//     currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-//     currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
-//     // Choose either "AM" or "PM" as appropriate
-//     var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-
-//     // Convert the hours component to 12-hour format if needed
-//     currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
-//     // Convert an hours component of "0" to "12"
-//     currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-
-//     // Compose the string for display
-//     var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-
-
-//     // Update the time display
-//     $(".digi_clock").html(currentTimeString)
-//     }
-
-// })
+    })
+})
